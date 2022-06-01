2bab091bbc744114c3d3d8adc9818ad79635cfe1
NicoStellwag <nico.stellwag@gmail.com>
2022-06-01 18:21:35 +0200

    setup completion with nvim-cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -56,17 +56,22 @@ return require('packer').startup(function(use)
 		'tami5/lspsaga.nvim',
 		config = get_setup("lspsaga")
 	})
-	--[[ use({
+	use({
+			'L3MON4D3/LuaSnip',
+			requires = {
+				'rafamadriz/friendly-snippets'
+			},
+			setup = get_setup("luasnip")
+	})
+	use({
 		'hrsh7th/nvim-cmp',
 		requires = {
 			'hrsh7th/cmp-nvim-lsp',
 			'hrsh7th/cmp-buffer',
 			'hrsh7th/cmp-path',
 			'hrsh7th/cmp-cmdline',
-			'L3MON4D3/LuaSnip',
 			'saadparwaiz1/cmp_luasnip',
-			'rafamadriz/friendly-snippets'
 		},
 		config = get_setup("nvim-cmp")
-	}) ]]
+	})
 end)
--- lua/setup/lspconfig.lua
+++ lua/setup/lspconfig.lua
@@ -3,7 +3,7 @@ local servers = { "clangd", "jsonls", "pyright", "sumneko_lua" }
 for _, lsp in pairs(servers) do
 	require('lspconfig')[lsp].setup {
 		on_attach = require("mappings").lspconfig_onattach_mappings,
-		-- capabilities = require("setup/nvim-cmp").capabilities
+		capabilities = require("setup/nvim-cmp").capabilities
 	}
 end
 
