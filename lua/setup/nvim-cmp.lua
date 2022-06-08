732a3e9ab8a5d2fa8788755bda3e0157fc1374e2
NicoStellwag <nico.stellwag@gmail.com>
2022-06-08 12:21:19 +0200

    make alt j quit insert mode and completion

--- lua/setup/nvim-cmp.lua
+++ lua/setup/nvim-cmp.lua
@@ -23,7 +23,7 @@ cmp.setup({
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<M-j>'] = cmp.mapping.abort(),
+		['<C-q>'] = cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 		['Tab'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
