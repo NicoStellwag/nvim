a45a84022d878d942506c2cee7fb497afc5c7082
NicoStellwag <nico.stellwag@gmail.com>
2022-06-16 14:52:39 +0200

    add path completion

--- lua/setup/nvim-cmp.lua
+++ lua/setup/nvim-cmp.lua
@@ -29,8 +29,9 @@ cmp.setup({
 	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
-		-- { name = 'vsnip' }, -- For vsnip users.
 		{ name = 'luasnip' }, -- For luasnip users.
+		{ name = 'path' },
+		-- { name = 'vsnip' }, -- For vsnip users.
 		-- { name = 'ultisnips' }, -- For ultisnips users.
 		-- { name = 'snippy' }, -- For snippy users.
 	}, {
