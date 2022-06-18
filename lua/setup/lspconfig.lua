325b4dac5deab344c4ebacf8919c71063f7512d4
NicoStellwag <nico.stellwag@gmail.com>
2022-06-18 11:21:31 +0200

    setup arduino lsp

--- lua/setup/lspconfig.lua
+++ lua/setup/lspconfig.lua
@@ -1,10 +1,27 @@
 local servers = require('setup/lsp-installer').get_servers()
 
 for _, lsp in pairs(servers) do
-	require('lspconfig')[lsp].setup {
+	local config = {
 		on_attach = require("mappings").lspconfig_onattach_mappings,
 		capabilities = require("setup/nvim-cmp").capabilities
 	}
+
+	-- server specific config
+	if (lsp == 'arduino_language_server') then
+		config.cmd = {
+			"arduino-language-server",
+			"-fqbn",
+			"arduino:mbed_nano:nano33ble",
+			"-cli-daemon-addr",
+			"localhost:50051",
+			"-cli-daemon-instance",
+			"1",
+			"-clangd",
+			"clangd"
+		}
+	end
+
+	require('lspconfig')[lsp].setup(config)
 end
 
 vim.diagnostic.config({
