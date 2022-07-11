1e6847e98b56996825a15a7388485ef466d7c8dc
NicoStellwag <nico.stellwag@gmail.com>
2022-07-11 11:50:52 +0200

    toggleterm fix open term bug

--- lua/setup/toggleterm.lua
+++ lua/setup/toggleterm.lua
@@ -13,35 +13,48 @@ local nmake_cmd = [[
 	powershell.exe -NoExit -Command "&{Import-Module """C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"""; Enter-VsDevShell 0638fbdc -SkipAutomaticLocation -DevCmdArguments """-arch=x64 -host_arch=x64"""; nmake}"
 ]]
 
+local default_term = Terminal:new({
+	direction = 'float'
+})
+local vsdevshell_term = nil
+local nmake_term = nil
+
+function _DEFAULT_TOGGLE()
+	default_term:toggle()
+end
+
 function _VSDEVSHELL_TOGGLE()
-	local curr_dir = get_dir_of_buffer_file()
-	if curr_dir == nil then
-		return
+	if vsdevshell_term == nil then
+		local curr_dir = get_dir_of_buffer_file()
+		if curr_dir == nil then
+			return
+		end
+		vsdevshell_term = Terminal:new({
+			direction = 'float',
+			cmd = vsdevshell_cmd,
+			dir = curr_dir
+		})
 	end
-	local vsdevshell_term = Terminal:new({
-		direction = 'float',
-		cmd = nmake_cmd,
-		dir = curr_dir
-	})
 	vsdevshell_term:toggle()
 end
+
 function _NMAKE_TOGGLE()
-	local curr_dir = get_dir_of_buffer_file()
-	if curr_dir == nil then
-		return
+	if nmake_term == nil then
+		local curr_dir = get_dir_of_buffer_file()
+		if curr_dir == nil then
+			return
+		end
+		nmake_term = Terminal:new({
+			direction = 'float',
+			cmd = nmake_cmd,
+			dir = curr_dir
+		})
 	end
-	local nmake_term = Terminal:new({
-		direction = 'float',
-		cmd = nmake_cmd,
-		dir = curr_dir
-	})
 	nmake_term:toggle()
 end
 
 -- general config
-local options = {
-	direction = 'float'
-}
+local options = {}
 if vim.fn.has('win32unix') then
 	options.shell = 'powershell'
 end
